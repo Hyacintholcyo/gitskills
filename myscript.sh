3a8d40e5b92ed45e5f1ec2d3c65dbb894459b674
lcy <olcyo@hotmail.com>
2019-06-24 17:37:32 +0800

    shell_learn_function

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-echo "What is your favourite OS?" 
-select var in "Linux" "Qnu Hurd" "HreeBSD" "Yther"; do 
+output1()
+{
+echo "It's function output1." 
+}
+output2()
+{
+echo "It's function output2." 
+}
+echo "Choose your function" 
+select var in "output1" "output2";do 
 	break 
 done 
-echo "You have selected $var"
-	
+case $var in 
+	"output1"*) 
+	output1;;
+	"output2"*)
+	output2;;
+	*) echo "ERROR"
+esac
